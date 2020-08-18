bea81f8ed1730ffef54813dfa39b7248f4f377c5
ImRajeevRanjan <68003373+ImRajeevRanjan@users.noreply.github.com>
2020-08-18 14:07:11 +0530

    new files uploaded

---
+++ Palindrome.java
@@ -0,0 +1,31 @@
+import java.util.Scanner;
+
+public class Palindrome {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("Enter the number to check it is palindrom or not:");
+		Scanner sc=new Scanner(System.in);
+		int x=sc.nextInt();
+		// 321 reverse is 123 so it is not palinrome number :
+		int y=x;
+		int r,num=0;
+		while(x>0)
+		{
+			r= x%10;
+			num=num*10 + r;
+			x = x/10;
+			
+		}
+		//System.out.println(num);
+		
+		if(num==y)
+			System.out.println("Yes it is Palindrome");
+		else
+			System.out.println("No it is not palindrome");
+		
+		
+	}
+
+}
