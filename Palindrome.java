55aa7ea9ed67aa9b6ea0944950a00ffbc757ca18
ImRajeevRanjan <68003373+ImRajeevRanjan@users.noreply.github.com>
2021-05-14 00:31:37 +0530

    Add files via upload

--- Palindrome.java
+++ Palindrome.java
@@ -1,35 +1,34 @@
-// trial file is edited
-// again file edited
-//edited in new branch
-
-import java.util.Scanner;
 
 public class Palindrome {
+	 public static boolean isPalindrome(String s) {
+	     
+		 
+		 int start = 0;
+		 int end = s.length()-1;
+		 
+		 while(start < end)
+		 {
+			 while(!Character.isLetterOrDigit(s.charAt(start)) && start < end)
+				 start++;
+			 
+			 while(!Character.isLetterOrDigit(s.charAt(end)) && start < end)
+				 end--;
+			 
+			 if(Character.toLowerCase(s.charAt(start)) != Character.toLowerCase(s.charAt(end)))
+			 {
+				return false; 
+			 }
+			 start++;
+			 end--;
+			 
+		 }
+		 return true;
+	    }
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		
-		System.out.println("Enter the number to check it is palindrom or not:");
-		Scanner sc=new Scanner(System.in);
-		int x=sc.nextInt();
-		// 321 reverse is 123 so it is not palinrome number :
-		int y=x;
-		int r,num=0;
-		while(x>0)
-		{
-			r= x%10;
-			num=num*10 + r;
-			x = x/10;
-			
-		}
-		//System.out.println(num);
-		
-		if(num==y)
-			System.out.println("Yes it is Palindrome");
-		else
-			System.out.println("No it is not palindrome");
-		
+
+		System.out.println(isPalindrome("raj,j,a;r"));
 		
 	}
-
 }
